d7e0cc7e40c90262ef2bfab88258ec0ca8a38fee
Philip Ahlberg <philipahlberg@gmail.com>
2026-04-19 19:48:43 +0200

    Setup

---
+++ target/debug/deps/pgdr-9f1e8047c1157c09.d
@@ -0,0 +1,5 @@
+/home/philipahlberg/pgdr/target/debug/deps/pgdr-9f1e8047c1157c09.d: crates/pgdr-cli/src/main.rs
+
+/home/philipahlberg/pgdr/target/debug/deps/libpgdr-9f1e8047c1157c09.rmeta: crates/pgdr-cli/src/main.rs
+
+crates/pgdr-cli/src/main.rs:
---
+++ target/debug/deps/pgdr-a87898c36d6f199d.d
@@ -0,0 +1,9 @@
+/home/philipahlberg/pgdr/target/debug/deps/pgdr-a87898c36d6f199d.d: crates/pgdr-cli/src/main.rs Cargo.toml
+
+/home/philipahlberg/pgdr/target/debug/deps/libpgdr-a87898c36d6f199d.rmeta: crates/pgdr-cli/src/main.rs Cargo.toml
+
+crates/pgdr-cli/src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
